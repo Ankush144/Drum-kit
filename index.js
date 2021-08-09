b2316a4471571ea2b92ffa187923a416e18ebb6c
Ankush <ankushpanchal144@gmail.com>
2021-08-09 19:06:25 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var size = document.querySelectorAll(".drum").length;
+for(var i =0;i<size;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",pressbutton);
+}
+
+document.addEventListener("keydown",presskey);
+function presskey(event){
+    eventHandler(event.key);
+    // animation(event.key);
+}
+function pressbutton(){
+    eventHandler(this.innerHTML);
+    // animation(this.innerHTML);
+}
+function eventHandler(x){
+    switch(x){
+        case 'w':{
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+       
+        }
+        case 'a':{
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        }
+        case 's':{
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        }
+        case 'd':{
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        }
+        case 'j':{
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        }
+        case 'l':{
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        }
+        case 'k':{
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        }
+    }
+}
+// function  animation(key){
+//     document.querySelector("."+key).classList.add("class_name");
+//     setTimeout(function(){document.querySelector("."+key).classList.remove("class_name");},200);
+// }
